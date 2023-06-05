ad9cc29e241546d07cb45ab2ebc0ccff66aec304
holson <bmholson@gmail.com>
2023-06-05 10:04:19 -0700

    more CORS

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ import generate_images
 app = Flask(__name__, static_folder='.', static_url_path='')
 app.config['SECRET_KEY'] = 'your secret key'
 socketio = SocketIO(app, cors_allowed_origins='*')
+CORS(app)
 
 styles = []
 
